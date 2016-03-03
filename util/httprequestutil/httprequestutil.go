2d649c924bda78d74274369574da8d133c76ee5a
wpxiong <wpxiong@gmail.com>
2016-03-04 01:05:54 +0900

    commit

--- util/httprequestutil/httprequestutil.go
+++ util/httprequestutil/httprequestutil.go
@@ -4,15 +4,36 @@ import (
   "github.com/wpxiong/beargo/log"
   "github.com/wpxiong/beargo/appcontext"
   "net/url"
+  "mime/multipart"
 )
 
 func init() {
   log.InitLog()
 }
 
+func MapMerge(dst, src url.Values) *url.Values{
+    var valuesMap url.Values = make(map[string][]string)
+    for key, value := range dst {
+        if valuesMap[key] == nil {
+          valuesMap[key] = make([]string,len(value))
+        }
+        valuesMap[key] = append(valuesMap[key],value...)
+    }
+    for key, value := range src {
+       if valuesMap[key] == nil {
+         valuesMap[key] = value
+       }else {
+         var slice1 []string = valuesMap[key]
+         valuesMap[key] = append(slice1,value...)
+       }
+    }
+    return &valuesMap
+}
+
 func ProcessHttpRequestParam(appContext *appcontext.AppContext) {
     var getParameter url.Values = appContext.Request.HttpRequest.URL.Query()
     var formParameter url.Values
+    var filesParameter map[string][]*multipart.FileHeader  
     contentType := appContext.Request.HttpRequest.Header.Get("Content-Type")
     switch contentType {
       case "application/x-www-form-urlencoded":
@@ -22,12 +43,23 @@ func ProcessHttpRequestParam(appContext *appcontext.AppContext) {
           formParameter = appContext.Request.HttpRequest.Form
         }
       case "multipart/form-data":
-        if err := appContext.Request.HttpRequest.ParseForm(); err != nil {
-         log.Error("process multipart/form-data error")
+        if err := appContext.Request.HttpRequest.ParseMultipartForm(32 << 20 /* 32 MB */); err != nil {
+            log.Error("process multipart/form-data error")
         } else {
-          formParameter = appContext.Request.HttpRequest.Form
+            formParameter = appContext.Request.HttpRequest.MultipartForm.Value
+            filesParameter = appContext.Request.HttpRequest.MultipartForm.File
         }
     }
-    log.Debug(getParameter)
-    log.Debug(formParameter)
+    formParameter = make(map[string][]string)
+    formParameter["xiong"] = []string{"gag"}
+    paramMap := MapMerge(getParameter,formParameter)
+    for key,value := range *paramMap {
+        if len(value) == 1{
+           appContext.Parameter[key] = value[0]
+        }else {
+           appContext.Parameter[key] = value
+        }
+    }
+    log.Debug(filesParameter)
+    log.Debug(appContext.Parameter)
 }
